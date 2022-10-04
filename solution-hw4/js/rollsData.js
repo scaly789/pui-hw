0e03abbca6e88a20f192e7f68424e623815e6775
Eric Gan <2019egan@gmail.com>
2022-10-03 20:33:22 -0400

    hw4

---
+++ solution-hw4/js/rollsData.js
@@ -0,0 +1,35 @@
+const rolls = {
+   "Original": {
+       "basePrice": 2.49,
+       "imageFile": "products/original-cinnamon-roll.jpg"
+   },
+   "Apple": {
+       "basePrice": 3.49,
+       "imageFile": "products/apple-cinnamon-roll.jpg"
+   },
+   "Raisin": {
+       "basePrice": 2.99,
+       "imageFile": "products/raisin-cinnamon-roll.jpg"
+   },
+   "Walnut": {
+       "basePrice": 3.49,
+       "imageFile": "products/walnut-cinnamon-roll.jpg"
+   },
+   "Double-Chocolate": {
+       "basePrice": 3.99,
+       "imageFile": "products/double-chocolate-cinnamon-roll.jpg"
+   },
+   "Strawberry": {
+       "basePrice": 3.99,
+       "imageFile": "products/strawberry-cinnamon-roll.jpg"
+   },  
+   "Vanilla": {
+      "basePrice": 2.49,
+      "imageFile": "products/vanilla-cinnamon-roll.jpg"
+  },
+  "Gluten-Free": {
+      "basePrice": 3.99,
+      "imageFile": "products/gluten-free-cinnamon-roll.jpg"
+  }  
+};
+
